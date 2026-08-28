c97c9b8c989a02d7a619a2303a6aabc3c95cd4d7
Cat4848 <catalinrol@gmail.com>
2026-08-28 18:44:12 +0100

    style: expandable background and border

--- frontend/src/pages/Hymns/Hymn/Hymn.tsx
+++ frontend/src/pages/Hymns/Hymn/Hymn.tsx
@@ -21,8 +21,8 @@ const Hymn = (props: Props) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <div className={globalStyles["text-align-left"]}>
-      <div className={styles["hymn-heading"]}>
+    <div>
+      <div className={`${styles["hymn-heading"]} ${globalStyles["text-align-left"]}`}>
         <div className={styles["title-and-author"]}>
           <div className={styles["chevron"]} onClick={() => setIsExpanded(!isExpanded)}>
             {isExpanded ? chevronRight : chevronDown}
@@ -35,29 +35,30 @@ const Hymn = (props: Props) => {
         <div>3 dots</div>
       </div>
       {isExpanded && (
-        <div>
-          <div>
-            <span>Author Extras</span>
-            {props.authorExtras}
+        <div className={styles["grid-in-foldable"]}>
+          <div className={styles["sections-in-foldable"]}>
+            <span>Author Extras:</span>
+            <span className={globalStyles["text-bold"]}>{props.authorExtras}</span>
           </div>
-          <div>
-            <span>Hymn Book</span>
-            {props.hymnBookName}
+          <div className={styles["sections-in-foldable"]}>
+            <span>Hymn Book:</span>
+            <span className={globalStyles["text-bold"]}>{props.hymnBookName}</span>
           </div>
-          <div>
-            <span>Number in Hymn Book</span>
-            {props.numberInHymnBook}
+          <div className={styles["sections-in-foldable"]}>
+            <span>Number in Hymn Book:</span>
+            <span className={globalStyles["text-bold"]}>{props.numberInHymnBook}</span>
           </div>
-          <div>
-            <span>Topic</span>
-            {props.topicName}
+          <div className={styles["sections-in-foldable"]}>
+            <span>Topic:</span>
+            <span className={globalStyles["text-bold"]}>{props.topicName}</span>
           </div>
-          <div>
-            <span>Label</span>
-            {props.labelName}
+          <div className={styles["sections-in-foldable"]}>
+            <span>Label:</span>
+            <span className={globalStyles["text-bold"]}>{props.labelName}</span>
           </div>
-          <div style={{ whiteSpace: "pre-wrap" }}>
-            <span>Lyrics</span>
+          <div></div>
+          <div className={`${styles["sections-in-foldable"]} ${styles["lyrics-in-foldable"]}`}>
+            <span>Lyrics:</span>
             {props.lyrics}
           </div>
         </div>
